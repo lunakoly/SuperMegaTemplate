2bef607ad92603de1971dcac9e8fb0dcec1d3a28
Nikolay Lunyak <lunyak.kolya@mail.ru>
2022-01-14 23:38:28 +0300

    Improve fixed attachments margins

--- content/code-samples/much-code.rs
+++ content/code-samples/much-code.rs
@@ -33,3 +33,39 @@ pub enum MessageProcessing {
     ProceedButWaiting,
     Stop,
 }
+
+pub mod json;
+pub mod bson;
+pub mod arson;
+
+use crate::{Result, Error, ErrorKind};
+
+pub const DEFAULT_PORT: u32 = 6969;
+
+pub trait ReadMessage<M> {
+    fn read_message(&mut self) -> Result<M>;
+}
+
+pub trait WriteMessage<M> {
+    fn write_message(&mut self, message: &M) -> Result<()>;
+}
+
+pub fn explain_common_error(error: &Error) -> String {
+    match &error.kind {
+        ErrorKind::Io { source: io_error } => match io_error.kind() {
+            // This particular error has a very
+            // specific meaning in terms of communication
+            std::io::ErrorKind::InvalidData => {
+                "Too much data for a single message, disconnecting".to_owned()
+            }
+            _ => format!("{}", error)
+        }
+        _ => format!("{}", error)
+    }
+}
+
+pub enum MessageProcessing {
+    Proceed,
+    ProceedButWaiting,
+    Stop,
+}
